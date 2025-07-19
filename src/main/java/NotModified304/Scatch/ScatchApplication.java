3305484fc725d52ae556b31c1293439ae2690ea3
doyeon <doyun20445@gmail.com>
2025-07-20 00:49:14 +0900

    initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/NotModified304/Scatch/ScatchApplication.java
@@ -0,0 +1,13 @@
+package NotModified304.Scatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NotModified304/Scatch/ScatchApplicationTests.java
@@ -0,0 +1,13 @@
+package NotModified304.Scatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
