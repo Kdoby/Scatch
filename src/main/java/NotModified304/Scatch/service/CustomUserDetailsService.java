764f9cc637b7db9379206ccbb61723fc761bb705
doyeon <doyun20445@gmail.com>
2025-08-07 16:54:23 +0900

    style: 주석 추가

--- src/main/java/NotModified304/Scatch/service/CustomUserDetailsService.java
+++ src/main/java/NotModified304/Scatch/service/CustomUserDetailsService.java
@@ -2,7 +2,7 @@ package NotModified304.Scatch.service;
 
 
 import NotModified304.Scatch.domain.Member;
-import NotModified304.Scatch.repository.UserRepository;
+import NotModified304.Scatch.repository.interfaces.UserRepository;
 import NotModified304.Scatch.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +20,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        // 해당하는 User의 데이터가 존재하면 UserDetails 객체로 만들어서 return
         return new CustomUserDetails(user);
     }
 }
