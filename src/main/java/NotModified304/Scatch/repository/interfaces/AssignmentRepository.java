54aacd8ab3eeceab5fb167a6a756f42c3a75ee2c
doyeon <doyun20445@gmail.com>
2025-08-07 22:38:55 +0900

    feat: assignment 등록, 수정, 삭제 구현

--- src/main/java/NotModified304/Scatch/repository/interfaces/AssignmentRepository.java
+++ src/main/java/NotModified304/Scatch/repository/interfaces/AssignmentRepository.java
@@ -1,10 +1,24 @@
 package NotModified304.Scatch.repository.interfaces;
 
 import NotModified304.Scatch.domain.Assignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public interface AssignmentRepository {
-    Assignment save(Assignment assignment);
-    List<Assignment> findAll(Long courseId);
+@Repository
+public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    List<Assignment> findByCourseId(Long courseId);
+    void deleteByCourseId(Long courseId);
+
+    @Query("SELECT a from Assignment a " +
+            "WHERE a.username =: username " +
+            "AND FUNCTION('DATE', a.deadline) =: targetDate")
+    List<Assignment> findByDate(@Param("username") String username,
+                                @Param("targetDate")LocalDate targetDate);
+
+
 }
