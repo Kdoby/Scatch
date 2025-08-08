3c68aeeedf877f8b2b4c696ac8bdb1b7361cde60
doyeon <doyun20445@gmail.com>
2025-08-09 00:12:22 +0900

    feat: 로그인한 사용자 인증 및 유효성 검사 코드 추가

--- src/main/java/NotModified304/Scatch/controller/CalendarApiController.java
+++ src/main/java/NotModified304/Scatch/controller/CalendarApiController.java
@@ -2,9 +2,11 @@ package NotModified304.Scatch.controller;
 
 import NotModified304.Scatch.dto.event.EventCreateRequest;
 import NotModified304.Scatch.dto.event.EventResponse;
+import NotModified304.Scatch.security.CustomUserDetails;
 import NotModified304.Scatch.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -19,8 +21,9 @@ public class CalendarApiController {
 
     // 일정 등록 (@RequestBody 누락)
     @PostMapping("/calendar")
-    public ResponseEntity<?> createEvent(@RequestBody EventCreateRequest request) {
-        eventService.registerEvent(request);
+    public ResponseEntity<?> createEvent(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                         @RequestBody EventCreateRequest req) {
+        eventService.registerEvent(userDetails.getUsername(), req);
 
         return ResponseEntity.ok(Map.of(
                 "success", true,
@@ -31,26 +34,28 @@ public class CalendarApiController {
     
     // 특정 년, 월에 해당하는 모든 일정 조회
     // http://localhost:8080/calendar/user123?year=2025&month=07
-    @GetMapping("/calendar/{userId}")
-    public List<EventResponse> getEventsByYearAndMonth(@PathVariable("userId") String userId,
-                                                          @RequestParam("year") Long year,
-                                                          @RequestParam("month") Long month) {
-        return eventService.findByYearAndMonth(userId, year, month);
+    @GetMapping("/calendar")
+    public List<EventResponse> getEventsByYearAndMonth(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                       @RequestParam("year") Long year,
+                                                       @RequestParam("month") Long month) {
+        return eventService.findByYearAndMonth(userDetails.getUsername(), year, month);
     }
 
     // 특정 날짜의 일정 조회
     // localhost:8080/calendar/event/user123?date=2025-07-20
-    @GetMapping("/calendar/event/{userId}")
-    public List<EventResponse> getOneEvent(@PathVariable("userId") String userId,
-                                              @RequestParam("date") LocalDate date) {
-        return eventService.findByDate(userId, date);
+    @GetMapping("/calendar/event")
+    public List<EventResponse> getOneEvent(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                           @RequestParam("date") LocalDate date) {
+        return eventService.findByDate(userDetails.getUsername(), date);
     }
     
     // 일정 업데이트
     @PutMapping("/calendar/{id}")
-    public ResponseEntity<?> updateEvent(@PathVariable("id") Long id,
+    public ResponseEntity<?> updateEvent(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                         @PathVariable("id") Long id,
                                          @RequestBody Map<String, Object> updates) {
-        eventService.updateEventById(id, updates);
+
+        eventService.updateEventById(userDetails.getUsername(), id, updates);
         
         return ResponseEntity.ok(Map.of(
                 "success", true,
@@ -60,8 +65,9 @@ public class CalendarApiController {
 
     // 일정 삭제
     @DeleteMapping("/calendar/{id}")
-    public ResponseEntity<?> deleteEvent(@PathVariable("id") Long id) {
-        eventService.deleteEvent(id);
+    public ResponseEntity<?> deleteEvent(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                         @PathVariable("id") Long id) {
+        eventService.deleteEvent(userDetails.getUsername(), id);
         return ResponseEntity.ok(Map.of(
                 "success", true,
                 "message", "일정 삭제 성공"
