5ce793dc2bd567b2ff0dca7e9ccd455203526057
doyeon <doyun20445@gmail.com>
2025-08-09 00:10:14 +0900

    refactor: 테이블 이름 변경

--- src/main/java/NotModified304/Scatch/service/AuthService.java
+++ src/main/java/NotModified304/Scatch/service/AuthService.java
@@ -5,7 +5,7 @@ import NotModified304.Scatch.dto.login.SigninRequest;
 import NotModified304.Scatch.dto.login.SignupRequest;
 import NotModified304.Scatch.dto.login.TokenResponse;
 import NotModified304.Scatch.jwt.JwtTokenProvider;
-import NotModified304.Scatch.repository.interfaces.UserRepository;
+import NotModified304.Scatch.repository.interfaces.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,7 +22,7 @@ import java.util.Optional;
 @Transactional
 @RequiredArgsConstructor
 public class AuthService {
-    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -36,14 +36,14 @@ public class AuthService {
                 .email(req.getEmail())
                 .build();
 
-        userRepository.save(user);
+        memberRepository.save(user);
 
         return user;
     }
 
     // 로그인
     public TokenResponse signin(SigninRequest req) {
-        Optional<Member> userOpt = userRepository.findByUsername(req.getUsername());
+        Optional<Member> userOpt = memberRepository.findByUsername(req.getUsername());
         if(userOpt.isEmpty() || !passwordEncoder.matches(req.getPassword(), userOpt.get().getPassword())) {
             throw new IllegalArgumentException("아이디 또는 비밀번호가 틀렸습니다.");
         }
@@ -59,7 +59,7 @@ public class AuthService {
         // refresh token DB에 저장
         userOpt.get().setRefreshToken(hashedRefreshToken);
         userOpt.get().setRefreshTokenExpiry(LocalDateTime.now().plusDays(14));
-        userRepository.save(userOpt.get());
+        memberRepository.save(userOpt.get());
 
         return new TokenResponse(accessToken, refreshToken);
     }
@@ -68,11 +68,11 @@ public class AuthService {
         if (!jwtTokenProvider.validateToken(refreshToken)) {
             String hashedToken = hashWithSHA256(refreshToken);
             // 만료되었거나 위조된 경우 db에서 삭제
-            userRepository.findByRefreshToken(hashedToken)
+            memberRepository.findByRefreshToken(hashedToken)
                     .ifPresent(user -> {
                         user.setRefreshToken(null);
                         user.setRefreshTokenExpiry(null);
-                        userRepository.save(user);
+                        memberRepository.save(user);
                     });
             throw new IllegalArgumentException("유효하지 않은 리프레시 토큰입니다.");
         }
@@ -80,7 +80,7 @@ public class AuthService {
         String username = jwtTokenProvider.getUsername(refreshToken);
 
         // (선택) DB에서 사용자 확인 - 이 과정을 통해 탈퇴 유저의 토큰도 막을 수 있음
-        Member user = userRepository.findByUsername(username)
+        Member user = memberRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
 
         if(!hashWithSHA256(refreshToken).equals(user.getRefreshToken())) {
@@ -91,7 +91,7 @@ public class AuthService {
     }
 
     private void validateDuplicationUser(SignupRequest req) {
-        userRepository.findByUsername(req.getUsername())
+        memberRepository.findByUsername(req.getUsername())
                 .ifPresent(u -> {
                     throw new IllegalArgumentException("이미 존재하는 ID 입니다.");
                 });
@@ -111,6 +111,6 @@ public class AuthService {
     public void logout(Member user) {
         user.setRefreshToken(null);
         user.setRefreshTokenExpiry(null);
-        userRepository.save(user);
+        memberRepository.save(user);
     }
 }
