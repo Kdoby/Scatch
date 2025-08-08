c4d510fdb1be87dfaa321e9c28b5ce51b29b39d8
doyeon <doyun20445@gmail.com>
2025-08-09 00:09:35 +0900

    refactor: 테이블 이름 변경

--- src/main/java/NotModified304/Scatch/repository/interfaces/MemberRepository.java
+++ src/main/java/NotModified304/Scatch/repository/interfaces/MemberRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
     Optional<Member> findByRefreshToken(String refreshToken);
 }
