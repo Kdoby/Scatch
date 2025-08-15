848bc034bb43389dd59775454d11e8a39ee21c18
doyeon <doyun20445@gmail.com>
2025-08-16 00:25:21 +0900

    feat: 인증 어노테이션 추가

--- src/main/java/NotModified304/Scatch/controller/StudyLogApiController.java
+++ src/main/java/NotModified304/Scatch/controller/StudyLogApiController.java
@@ -3,9 +3,11 @@ package NotModified304.Scatch.controller;
 import NotModified304.Scatch.dto.studyLog.StudyLogCreateRequest;
 import NotModified304.Scatch.dto.studyLog.StudyLogResponse;
 import NotModified304.Scatch.dto.studyLog.StudyLogUpdateRequest;
+import NotModified304.Scatch.security.CustomUserDetails;
 import NotModified304.Scatch.service.StudyLogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -15,12 +17,15 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 public class StudyLogApiController {
+
     private final StudyLogService studyLogService;
 
     // 공부 기록 등록
     @PostMapping("/todo/log")
-    public ResponseEntity<?> createStudyLog(@RequestBody StudyLogCreateRequest request) {
-        studyLogService.registerStudyLog(request);
+    public ResponseEntity<?> createStudyLog(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                            @RequestBody StudyLogCreateRequest request) {
+
+        studyLogService.registerStudyLog(userDetails.getUsername(), request);
 
         return ResponseEntity.ok(Map.of(
                 "success", true,
@@ -30,9 +35,11 @@ public class StudyLogApiController {
 
     // 공부 기록 수정
    @PutMapping("/todo/log/{id}")
-    public ResponseEntity<Map<String, Object>> updateStudyLog(@PathVariable("id") Long id,
-                                            @RequestBody StudyLogUpdateRequest request) {
-        studyLogService.updateStudyLog(id, request);
+    public ResponseEntity<Map<String, Object>> updateStudyLog(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                              @PathVariable("id") Long id,
+                                                              @RequestBody StudyLogUpdateRequest request) {
+
+        studyLogService.updateStudyLog(userDetails.getUsername(), id, request);
 
         return ResponseEntity.ok(Map.of(
                 "success", true,
@@ -42,8 +49,10 @@ public class StudyLogApiController {
 
     // 공부 기록 삭제
     @DeleteMapping("/todo/log/{id}")
-    public ResponseEntity<?> deleteStudyLog(@PathVariable("id") Long id) {
-        studyLogService.removeStudyLog(id);
+    public ResponseEntity<?> deleteStudyLog(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                            @PathVariable("id") Long id) {
+
+        studyLogService.removeStudyLog(userDetails.getUsername(), id);
 
         return ResponseEntity.ok(Map.of(
                 "success", true,
@@ -52,9 +61,10 @@ public class StudyLogApiController {
     }
 
     // 특정 날짜의 공부 기록 조회
-    @GetMapping("/todo/log/{userId}/{date}")
-    public ResponseEntity<List<StudyLogResponse>> getStudyLogList(@PathVariable("userId") String userId,
+    @GetMapping("/todo/log/{date}")
+    public ResponseEntity<List<StudyLogResponse>> getStudyLogList(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                                   @PathVariable("date") LocalDate date) {
-        return ResponseEntity.ok(studyLogService.getStudyLogsByDate(userId, date));
+
+        return ResponseEntity.ok(studyLogService.getStudyLogsByDate(userDetails.getUsername(), date));
     }
 }
