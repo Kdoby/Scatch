93fc987b29e6fad2b1b4a5c299b2d546b4a2c179
doyeon <doyun20445@gmail.com>
2025-08-16 00:21:15 +0900

    refactor: domain userId -> username으로 변경

--- src/main/java/NotModified304/Scatch/domain/Lesson.java
+++ src/main/java/NotModified304/Scatch/domain/Lesson.java
@@ -19,8 +19,8 @@ public class Lesson {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
-    private String userId;
+    @Column(name = "username", nullable = false)
+    private String username;
 
     @Column(name = "content")
     private String content;
