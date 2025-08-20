fb3e0697dcef82700085f04f5f2be9ff4bdcc476
yuminmi <minming0400@naver.com>
2025-08-20 18:50:23 +0900

    fix: continue 버튼 범위 수정

--- src/main/frontend/src/timeattack/TimeAttackMain.js
+++ src/main/frontend/src/timeattack/TimeAttackMain.js
@@ -249,7 +249,9 @@ function StudyTimer ({targetTime, mode, onStop, onPause, onResume, onContinue, o
             <h2>{String(hours).padStart(2, "0")} : {String(minutes).padStart(2, "0")} : {String(seconds).padStart(2, "0")}</h2>
             {mode === "normal" ? (
                 <>
-                    <p onClick={() => onContinue(isFinished)} style={{textDecoration: "underline", cursor:"pointer"}}>continue</p>
+                    <div style={{display: "flex", justifyContent: "center"}}>
+                        <p onClick={() => onContinue(isFinished)} style={{textDecoration: "underline", cursor:"pointer", width: "fit-content"}}>continue</p>
+                    </div>
                     <div>
                         <button onClick={() => {
                             if (!isPaused) {
