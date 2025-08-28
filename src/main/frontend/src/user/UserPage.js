d2aee4325334bd9dbd10aa9f8bcfdd274a2835a4
eheka78 <gemddkim22@gmail.com>
2025-08-29 04:55:36 +0900

    feat: 마이페이지에 정보 변경 기능 삽입

--- src/main/frontend/src/user/UserPage.js
+++ src/main/frontend/src/user/UserPage.js
@@ -5,45 +5,196 @@ import api from '../api';
 import {useEffect, useState} from "react";
 
 export default function UserPage () {
-    const [palette, setPalette] = useState(1);
+    const [myInfo, setMyInfo] = useState({
+        paletteNumber: 1,
+        nickname: '',
+        intro: '',
+        profileImagePath: ''
+    });
+    const [pwd, setPwd] = useState({
+       currentPassword: '',
+       newPassword: ''
+   });
 
-    // 팔레트 조회
-    const fetchPalette = async () => {
+
+    // 내 정보 조회
+    const fetchMyInfo = async () => {
         try {
-            const res = await api.get('/member/palette');
+            const res = await api.get('/member/profile');
 
-            //alert(res.data.message + res.data.data);
-            setPalette(res.data.data);
-            console.log("fetchPalette 받아오기: ", res.data);
+            setMyInfo(res.data);
+            console.log("fetchMyInfo 받아오기: ", res.data);
         } catch (e) {
             console.error("fail fetch: ", e);
         }
     }
 
-    // 팔레트 색상 변경
-    const changePalette = async (num) => {
-        try {
-            await api.put( '/member/palette', null, { params: { number: num } } );
+    // 한 줄 소개, 닉네임 변경
+    const uploadIntroNicknamePalette = async () => {
+        console.log("수정: ", myInfo);
 
-            //alert(res.data.message + res.data.data);
-            setPalette(num);
-            console.log(num + "으로 Palette 변경됨");
+        try {
+            const res = await api.put('/member/profile', {
+                paletteNumber: myInfo.paletteNumber,
+                nickname: myInfo.nickname,
+                intro: myInfo.intro
+            });
+
+            alert(res.data);
         } catch (e) {
             console.error("fail fetch: ", e);
         }
+
+        // 변경하고 myInfo 다시 받아오기
+        fetchMyInfo();
+    }
+
+    // 팔레트 변경
+    const changePalette = async (pn) => {
+        console.log("수정: ", myInfo);
+
+        try {
+            const res = await api.put('/member/profile', {
+                paletteNumber: pn,
+                nickname: myInfo.nickname,
+                intro: myInfo.intro
+            });
+
+            alert(res.data);
+        } catch (e) {
+            console.error("fail fetch: ", e);
+        }
+
+        // 변경하고 myInfo 다시 받아오기
+        fetchMyInfo();
+    }
+
+    // 비밀번호 변경
+    const changePassword = async (pn) => {
+        console.log("수정: ", myInfo);
+
+        try {
+            const res = await api.put('/auth/password', {
+                currentPassword: pwd.currentPassword,
+                newPassword: pwd.newPassword
+            });
+
+            alert(res.data);
+        } catch (e) {
+            console.error("fail fetch: ", e);
+        }
+
+        // 변경하고 myInfo 다시 받아오기
+        fetchMyInfo();
     }
 
     useEffect(() => {
-        fetchPalette();
+        fetchMyInfo();
     }, []);
+
+    useEffect(() => {
+        console.log("111111111: " + myInfo);
+    }, [myInfo]);
 
     return (
         <div style={{textAlign:"left"}}>
             <div style={{ margin:"30px 0", fontWeight:"bold", fontSize:"30px" }}>설정</div>
-            <div style={{ display: "grid", gridTemplateColumns: "1fr 3fr", fontSize:"20px" }}>
+            <div>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        uploadIntroNicknamePalette();
+                    }}
+                     style={{ display: "grid", gridTemplateColumns: "1fr 3fr", gap: "20px", fontSize:"20px" }}
+                >
+                <div>프로필 사진</div>
+                <div>
+                    <div style={{ width: "10%" }}>
+                        <img src={ `http://localhost:8080/uploads/${myInfo.storedFileName}` }
+                             alt={ myInfo.originalFileName } />
+
+                    </div>
+                    <input type="file"
+                           style={{ width:"100%", padding: "10px" }} />
+                </div>
+                <div></div>
+                <div>
+                    <button type="submit" style={{ width:"100%", padding: "10px" }}>프로필 save</button>
+                </div>
+                </form>
+
+                <br /><br />
+
+                <form onSubmit={(e) => {
+                        e.preventDefault();
+                        uploadIntroNicknamePalette();
+                      }}
+                      style={{ display: "grid", gridTemplateColumns: "1fr 3fr", gap: "20px", fontSize:"20px" }}
+                >
+                    <div>한 줄 소개</div>
+                    <div>
+                        <input
+                            defaultValue={myInfo.intro}
+                            onChange={(e) => setMyInfo({ ...myInfo, intro: e.target.value })}
+                            style={{ width: "100%", padding: "10px" }}
+                        />
+                    </div>
+
+                    <div>Nickname</div>
+                    <div>
+                        <input
+                            defaultValue={myInfo.nickname}
+                            onChange={(e) => setMyInfo({ ...myInfo, nickname: e.target.value })}
+                            style={{ width: "100%", padding: "10px" }}
+                        />
+                    </div>
+
+                    <div></div>
+                    <div>
+                        <button type="submit" style={{ width:"100%", padding: "10px" }}>한 줄 소개, 닉네임 save</button>
+                    </div>
+                </form>
+
+                <form onSubmit={(e) => {
+                        e.preventDefault();
+                        changePassword();
+                      }}
+                      style={{ display: "grid", gridTemplateColumns: "1fr 3fr", gap: "20px", fontSize:"20px" }}
+                >
+                    <div>비밀번호</div>
+                    <div>
+                        기존 비밀번호:
+                        <input style={{ width:"100%", padding: "10px" }}
+                               onChange={(e) => setPwd({ ...pwd, currentPassword: e.target.value })}/>
+
+                        <br />
+
+                        새 비밀번호:
+                            <input style={{ width:"100%", padding: "10px" }}
+                                   onChange={(e) => setPwd({ ...pwd, newPassword: e.target.value })}/>
+                    </div>
+
+                    <div></div>
+                    <div>
+                        <button type="submit" style={{ width:"100%", padding: "10px" }}>pwd save</button>
+                    </div>
+                </form>
+
+                <div></div><div></div>
+
+                <div></div><div></div>
+
+
+                <div></div><div></div><div></div><div></div><div></div><div></div>
+
                 <div>팔레트 설정</div>
                 <div>
-                    <select value={String(palette)} onChange={(e) => changePalette(Number(e.target.value))}>
+                    <select value={Number(myInfo.paletteNumber)}
+                            onChange={(e) => {
+                                changePalette(e.target.value);
+                            }}
+                    >
+
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -54,8 +205,9 @@ export default function UserPage () {
                 <div>
                 </div>
                 <div>
-                    <Palette paletteN={palette} clickTF={true}/>
+                    <Palette paletteN={myInfo.paletteNumber} clickTF={true}/>
                 </div>
+
             </div>
         </div>
     );
