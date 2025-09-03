71d620ac44f3d929be1ae5dc2591dd5d87000ded
doyeon <doyun20445@gmail.com>
2025-09-03 14:49:23 +0900

    fix: 프로필 사진 불러오기 수정

--- src/main/java/NotModified304/Scatch/config/WebConfig.java
+++ src/main/java/NotModified304/Scatch/config/WebConfig.java
@@ -1,16 +1,26 @@
 package NotModified304.Scatch.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
+@EnableWebSecurity
+// @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    // <img src="/uploads/basic.png"> → http://localhost:8080/uploads/xxx.jpg → {프로젝트}/uploads/xxx.jpg
+    // <img src="/uploads/basic.png"> → kkxxx.jpg → {프로젝트}/uploads/xxx.jpg
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/")
+                .setCachePeriod(60 * 10 * 6)	//1시간
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
     }
 }
